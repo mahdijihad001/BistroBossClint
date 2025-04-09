8bf3cca5c5f25501f5c6764c74c4f4c157fb9b01
mahdiJihad <mahdijihad001@gmail.com>
2025-04-09 12:51:07 +0600

    Host bistro Boss Clint

---
+++ src/Router/Router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path : "/",
+        element : <h1>Hello World</h1>
+    }
+]);
+
+
+export default router
